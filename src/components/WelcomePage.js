1d1c900307cfc26bb044a922f59c6f8c4326b4ce
Nhinh Dao <nhinhdao.vn@gmail.com>
2019-02-26 14:23:53 -0500

    Remove logic out of presentation Welcome Page

--- src/components/WelcomePage.js
+++ src/components/WelcomePage.js
@@ -1,40 +1,25 @@
 import React, { Component } from 'react';
 import { mySearchQuery, singleSearchQuery } from '../actions/APIsearch';
 import SearchForm from '../containers/SearchForm';
+import RenderSearchData from './RenderSearchData';
+import RenderSinglePlace from './RenderSinglePlace';
+
 import { connect } from 'react-redux';
 
 class WelcomePage extends Component {
-  handleClick = id => {
-    console.log("i am here");
-    
-    let url = `https://api.foursquare.com/v2/venues/${id}?client_id=${process.env.REACT_APP_CLIENT_ID}&client_secret=${process.env.REACT_APP_CLIENT_SECRET}&v=20160201&near=
-    ${this.state.location}&query=${this.state.searchQuery}&limit=20`;
-    this.props.singleSearchQuery(url);
-  }
-
   render() {
-    const {mySearchData, myPlace} = this.props
     return (
       <div>
-        <SearchForm handleSearch={this.props.mySearchQuery}/>
-        <div>
-          <ul>{mySearchData.map(data => <li key={data.id} onClick={() => this.handleClick(data.id)}>{data.category} - {data.name}</li>)}</ul>
-          <hr />
-          <h3>{myPlace.name}</h3>
-          <img src={myPlace.photo} alt={myPlace.name} />
-          <p>{myPlace.rating}</p>
-          <p>Location: {myPlace.location}</p>
-          <p>Contact: {myPlace.contact}</p>
-          <p>Tips: {myPlace.tips}</p>
-        </div>
+        <SearchForm handleSearch={this.props.mySearchQuery} />
+        {this.props.mySearchData.map(data => <RenderSearchData key={data.id} handleSearch={this.props.singleSearchQuery} place={data}/> )}
+        <hr />
+        <RenderSinglePlace myPlace={this.props.myPlace} />
       </div>
     )
   }
 }
 
 const mapStateToProps = state => {
-  console.log(state);
-  
   return {
     mySearchData: state.mySearch.searchData,
     myPlace: state.singleSearch.searchPlace
