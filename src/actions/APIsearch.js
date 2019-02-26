7e2fb445b879a6df68dd9d0172fee99037ed7559
Nhinh Dao <nhinhdao.vn@gmail.com>
2019-02-26 13:44:55 -0500

    Add Recommended Search Action

--- src/actions/APIsearch.js
+++ src/actions/APIsearch.js
@@ -15,3 +15,12 @@ export function singleSearchQuery(url) {
       .then(data => dispatch({ type: 'FETCH_SINGLE_SEARCH_QUERY', payload: data.response.venue }));
   }
 }
+
+export function recommendedSearchQuery(url) {
+  return dispatch => {
+    dispatch({ type: "LOADING_QUERY" });
+    return fetch(url)
+      .then(response => response.json())
+      .then(data => dispatch({ type: 'FETCH_RECOMMENDED_SEARCH_QUERY', payload: data.response.venues }));
+  }
+}
