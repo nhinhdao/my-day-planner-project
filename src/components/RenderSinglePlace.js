e460328e23035b31e55eded97b0280eaf1f8a597
Nhinh Dao <nhinhdao.vn@gmail.com>
2019-02-26 14:23:24 -0500

    Add render file for single place

---
+++ src/components/RenderSinglePlace.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+class RenderSinglePlace extends Component {
+  render() {
+    const {myPlace} = this.props
+    return (
+      <div>
+        <h3>{myPlace.name}</h3>
+        <img src={myPlace.photo} alt={myPlace.name} />
+        <p>{myPlace.rating}</p>
+        <p>Location: {myPlace.location}</p>
+        <p>Contact: {myPlace.contact}</p>
+        <p>Tips: {myPlace.tips}</p>
+      </div>
+    )
+  }
+}
+        
+export default RenderSinglePlace;
