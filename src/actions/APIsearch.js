eeb5b456e3bc4777c79bdb75cc2c27248a04868c
Nhinh Dao <nhinhdao.vn@gmail.com>
2019-02-27 12:47:17 -0500

    Added review search action and reducer

--- src/actions/APIsearch.js
+++ src/actions/APIsearch.js
@@ -1,26 +1,59 @@
 export function mySearchQuery(url) {
+  const obj = {
+    method: 'GET',
+    headers: {
+      'Authorization': `Bearer ${process.env.REACT_APP_YELP_TOKEN}`
+    }
+  }
   return dispatch => {
     dispatch({ type: "LOADING_QUERY" });
-    return fetch(url)
+    return fetch(url, obj)
       .then(response => response.json())
-      .then(data => dispatch({ type: 'FETCH_MY_SEARCH_QUERY', payload: data.response.venues }));
+      .then(data => dispatch({ type: 'FETCH_MY_SEARCH_QUERY', payload: data.businesses }))
   }
 }
 
 export function singleSearchQuery(url) {
+  const obj = {
+    method: 'GET',
+    headers: {
+      'Authorization': `Bearer ${process.env.REACT_APP_YELP_TOKEN}`
+    }
+  }
   return dispatch => {
     dispatch({ type: "LOADING_QUERY" });
-    return fetch(url)
+    return fetch(url, obj)
       .then(response => response.json())
-      .then(data => dispatch({ type: 'FETCH_SINGLE_SEARCH_QUERY', payload: data.response.venue }));
+      .then(data => dispatch({ type: 'FETCH_SINGLE_SEARCH_QUERY', payload: data}));
   }
 }
 
 export function recommendedSearchQuery(url) {
+  const obj = {
+    method: 'GET',
+    headers: {
+      'Authorization': `Bearer ${process.env.REACT_APP_YELP_TOKEN}`
+    }
+  }
   return dispatch => {
     dispatch({ type: "LOADING_QUERY" });
-    return fetch(url)
+    return fetch(url, obj)
       .then(response => response.json())
       .then(data => dispatch({ type: 'FETCH_RECOMMENDED_SEARCH_QUERY', payload: data.response.venues }));
+  }
+}
+
+export function reviewSearchQuery(url) {
+  const obj = {
+    method: 'GET',
+    headers: {
+      'Authorization': `Bearer ${process.env.REACT_APP_YELP_TOKEN}`
+    }
+  }
+  return dispatch => {
+    dispatch({ type: "LOADING_QUERY" });
+    return fetch(url, obj)
+      .then(response => response.json())
+      .then(data => dispatch({ type: 'FETCH_REVIEW_SEARCH_QUERY', payload: data.reviews }));
   }
 }
--- src/components/RenderSinglePlace.js
+++ src/components/RenderSinglePlace.js
@@ -1,20 +1,52 @@
 import React, { Component } from 'react';
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import { Carousel } from 'react-responsive-carousel';
+import { reviewSearchQuery } from '../actions/APIsearch';
+import { connect } from 'react-redux';
+import RenderReviews from './RenderReviews';
 
 class RenderSinglePlace extends Component {
+  state={isReviewClicked: false}
+  handleReviews = (id) => {
+    let url = `https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/${id}/reviews`;
+    this.setState({isReviewClicked: true})
+    this.props.reviewSearchQuery(url);
+  }
+
+  handleAddFavorite = () => {
+
+  }
+
   render() {
-    const {myPlace} = this.props
+    const { myPlace } = this.props;
+    const imageList = myPlace.photos ?
+      <Carousel dynamicHeight={false} showThumbs={false} infiniteLoop={true}>
+        {myPlace.photos.map((photo, index) =>
+          <div key={index} className="side-crop" ><img src={photo} alt={myPlace.name}/></div>)}
+      </Carousel> : "No Images Available";
+    
     return (
       <div>
         <h3>{myPlace.name}</h3>
-        <img src={myPlace.photo} alt={myPlace.name} />
-        <p>{myPlace.rating}</p>
-        <p>{myPlace.description}</p>
+        {imageList}
+        <p>Rating: {myPlace.rating}</p>
+        <p>Category: {myPlace.category}</p>
         <p>Location: {myPlace.location}</p>
         <p>Contact: {myPlace.contact}</p>
-        <p>Tips: {myPlace.tips}</p>
+        <button onClick={() => this.handleReviews(myPlace.id)}>View reviews</button>
+        <button onClick={this.handleAddFavorite}>Add to my favorite list</button>
+        {this.state.isReviewClicked ? <RenderReviews reviews={this.props.reviews} /> : null}
       </div>
     )
   }
 }
+
+const mapStateToProps = state => {
+  return {reviews: state.reviewsSearch.reviews}
+}
+
+const mapDispatchToProps = dispatch => {
+  return { reviewSearchQuery: url => dispatch(reviewSearchQuery(url))}
+}
         
-export default RenderSinglePlace;
+export default connect(mapStateToProps, mapDispatchToProps)(RenderSinglePlace);
